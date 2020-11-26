51a55f6c9f1cde1564f8f62658a8ad25588d93d6
Greyacey <ajibade.graceyetunde.com>
2020-11-27 00:31:34 +0100

    update reciever number

--- src/app/home-pg/home-pg.component.ts
+++ src/app/home-pg/home-pg.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{ AcctextService } from '../acctext.service'
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'app-home-pg',
@@ -8,17 +9,37 @@ import{ AcctextService } from '../acctext.service'
 })
 export class HomePgComponent implements OnInit {
 
+  data:[][];
   constructor(private acctext:AcctextService) { }
-  onSubmit(data)
+  onSubmit(test)
   {
-    //console.warn(data)
-    this.acctext.saveData(data).subscribe((result)=>{
-      console.warn("result is here", result)
-    })
+    test.numfile = this.data
+    console.warn(test)
+    
   }
 
   ngOnInit(): void {
     
+  }
+  onFileChange(evt: any){
+    const target : DataTransfer = <DataTransfer>(evt.target);
+
+    if (target.files.length !== 1) throw new Error('Cannot use multile files');
+
+    const reader: FileReader = new FileReader();
+
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary'});
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+      this.data = (XLSX.utils.sheet_to_json(ws, { header: 1}))
+      console.log(this.data)
+
+    };
+
+    reader.readAsBinaryString(target.files[0]);
+
   }
   
 
